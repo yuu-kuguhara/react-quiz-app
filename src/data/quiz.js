6395d10847ca525c21c22ba26cfd472cd143636d
yuu-kuguhara <you20010819@gmail.com>
2025-06-15 22:38:53 +0900

    問題文を更新 9問実装 ※プラス1問追加予定(記述式)

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,25 +1,42 @@
 const quizData = [
     {
-        question: "ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ",
-        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        question: "「千と千尋の神隠し」で、千尋が働くことになった湯屋の名前は？",
+        options: ["油屋", "銭屋", "薬屋", "紙屋"],
         answerIndex: 0
     }, {
-        question: "Question 2",
-        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-        answerIndex: 1
-    }, {
-        question: "Question 3",
-        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-        answerIndex: 2
-    }, {
-        question: "Question 4",
-        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        question: "「ハウルの動く城」で、ハウルの城の動力源となっている火の悪魔の名前は？",
+        options: ["マルクル", "ヒン", "カブ", "カルシファー"],
         answerIndex: 3
     }, {
-        question: "Question 5",
-        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        question: "「風の谷のナウシカ」で、腐海の森に生息する巨大な蟲の王様は？",
+        options: ["ヘビケラ", "王蟲", "ウシアブ", "ユパ"],
+        answerIndex: 1
+    }, {
+        question: "スタジオジブリ作品の多くで音楽を担当している作曲家は誰？",
+        options: ["坂本龍一", "久石譲", "菅野よう子", "冨田勲"],
+        answerIndex: 1
+    }, {
+        question: "「耳をすませば」で、主人公の月島雫が物語を書くきっかけとなった、猫の人形の名前は？",
+        options: ["ムタ", "バロン", "ルナルド", "トト"],
+        answerIndex: 1
+    }, {
+        question: "「千と千尋の神隠し」で、千尋の両親らしき豚を見つけるために挑戦した試練で、用意された豚の数は何頭でしたか？",
+        options: ["6頭", "10頭", "12頭", "20頭"],
+        answerIndex: 3
+    }, {
+        question: "「天空の城ラピュタ」で、ドーラ一家が所有する飛行船の名前は何ですか？",
+        options: ["タイガーモス号", "ゴリアテ", "ノア", "インデバー"],
+        answerIndex: 0
+    }, {
+        question: "「もののけ姫」で、アシタカが旅立つ原因となったタタリ神は何に侵されたものだったでしょう？",
+        options: ["鉄", "憎しみ", "毒", "病"],
+        answerIndex: 1
+    }, {
+        question: "「魔女の宅急便」で、キキが最初に空を飛んだ時に一緒にいたのは誰ですか？",
+        options: ["おソノさん", "ジジ", "トンボ", "ウルスラ"],
         answerIndex: 1
     }
 ];
-
+//問10："「猫の恩返し」に登場するバロン男爵の正式名称は何ですか？"→"フンベルトフォンジッキンゲン(男爵)"
+// 上記は記述式で実装、正解は2択
 export default quizData
